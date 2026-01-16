b124bc3d6e8f92650c19aee5db2440d2e105a981
Alejandro Espartal <alejandro.espartal@nunsys.com>
2026-01-16 13:48:19 +0100

    Configuración de despliegue con Docker

--- src/main/java/com/ejemplo/webapp_azure/controller/CitaController.java
+++ src/main/java/com/ejemplo/webapp_azure/controller/CitaController.java
@@ -3,6 +3,7 @@ package com.ejemplo.webapp_azure.controller;
 import com.ejemplo.webapp_azure.model.Cita;
 import com.ejemplo.webapp_azure.repository.CitaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/citas")
+@CrossOrigin(origins = "*")
 public class CitaController {
 
   @Autowired
