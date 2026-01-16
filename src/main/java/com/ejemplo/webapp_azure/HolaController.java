6b2ee2bb96b01f88fb02e6c72f199913a66a095d
Alejandro Espartal <alejandro.espartal@nunsys.com>
2026-01-16 12:36:38 +0100

    feat: add v2 to HolaController

--- src/main/java/com/ejemplo/webapp_azure/HolaController.java
+++ src/main/java/com/ejemplo/webapp_azure/HolaController.java
@@ -9,7 +9,7 @@ public class HolaController {
   @GetMapping("/")
   public String home() {
     return "<h1>¡Bienvenido a Demo App!</h1>" +
-            "<p>Aplicación Spring Boot desplegada en Oracle Cloud.</p>" +
+            "<p>Aplicación Spring Boot desplegada en Oracle Cloud. v2</p>" +
             "<p>Status: <b>Activo y corriendo</b></p>";
   }
 
